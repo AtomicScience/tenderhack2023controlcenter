2c6a0686a75a2af53ae60bfc6df84380bd67ee9e
Дмитрий Греблюк <d.grebliuk@vk.team>
2023-10-22 15:00:36 +0300

    Upscaled iframe

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -2,13 +2,16 @@ import { Flex, Stack, Title } from '@mantine/core';
 
 export const DasboardPage = () => {
     return (
-      <Stack>
-        <Title order={1} size="xl" fw={700}>
-          Аналитика
-        </Title>
-        <Flex gap="sm" direction="column" rowGap={8}>
-          <iframe className='h-screen' src="http://10.9.67.97:3000/public-dashboards/b92ea8887dc341a19e998b514569903a?orgId=1&theme=light" width="100%" height="auto" frameBorder="0"></iframe>        
-        </Flex>
-      </Stack>
-    )
+      <iframe
+        className="h-screen -m-4"
+        style={{
+          width:  "calc(100% + 32px)",
+          height: "calc(100vh - 60px)",
+        }}
+        src="http://10.9.67.97:3000/public-dashboards/b92ea8887dc341a19e998b514569903a?orgId=1&theme=light"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+      ></iframe>
+    );
 }
